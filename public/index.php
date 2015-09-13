b757af409924c8df7b9772c2a6b7c6ab281dd1b0
underdogg <drosbeds@xs4all.nl>
2015-09-13 15:38:14 +0200

    Moving around some files, adding a .gitignore for files and directories that shouldn't be added to git

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
-require 'config.php';
-require 'util/Auth.php';
+define('APP_PATH', realpath('../app/'));
+
+require_once APP_PATH.'/config/config.php';
+require_once APP_PATH.'/util/Auth.php';
 
 // Also spl_autoload_register (Take a look at it if you like)
 function __autoload($class) {
