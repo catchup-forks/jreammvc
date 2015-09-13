33e98705b80c6a2dfa19057eb93d70926d7d1b80
underdogg <drosbeds@xs4all.nl>
2015-09-13 19:20:12 +0200

    Show all the notes from the database in a nice CSS table

--- app/views/note/index.php
+++ app/views/note/index.php
@@ -1,34 +1,46 @@
+Edit your notes:
+
+
 <h1>Note</h1>
 
-<form method="post" action="<?php echo URL;?>note/create">
-    <label>title</label><input type="text" name="title" /><br />
-    <label>content</label><textarea name="content"></textarea><br />
-    <label>&nbsp;</label><input type="submit" />
+<form method="post" action="<?php echo URL; ?>note/create">
+  <label>title</label><input type="text" name="title"/><br/>
+  <label>content</label><textarea name="content"></textarea><br/>
+  <label>&nbsp;</label><input type="submit"/>
 </form>
 
-<hr />
+<hr/>
 
-<table>
-<?php
-    foreach($this->noteList as $key => $value) {
-        echo '<tr>';
-        echo '<td>' . $value['title'] . '</td>';
-        echo '<td>' . $value['date_added'] . '</td>';
-        echo '<td><a href="'. URL . 'note/edit/' . $value['noteid'].'">Edit</a></td>';
-        echo '<td><a class="delete" href="'. URL . 'note/delete/' . $value['noteid'].'">Delete</a></td>';
-        echo '</tr>';
-    }
-?>
+<table class="table table-striped">
+<thead>
+<th>Title</th>
+<th>Date Added</th>
+<th colspan="2">Actions</th>
+</thead>
+<tbody>
+  <?php
+  foreach ($this->noteList as $key => $value) {
+    ?>
+    <tr>
+      <td><?php echo $value['title']; ?></td>
+      <td><?php echo $value['date_added']; ?></td>
+      <td><a href="<?php echo URL . 'note/edit/' . $value['noteid']; ?>">Edit</a></td>
+      <td><a class="delete" href="<?php echo URL . 'note/delete/' . $value['noteid']; ?>">Delete</a></td>
+    </tr>
+  <?php
+  }
+  ?>
+</tbody>
 </table>
 
 <script>
-$(function() {
-    
-    $('.delete').click(function(e) {
-        var c = confirm("Are you sure you want to delete?");
-        if (c == false) return false;
-        
+  $(function () {
+
+    $('.delete').click(function (e) {
+      var c = confirm("Are you sure you want to delete?");
+      if (c == false) return false;
+
     });
-    
-});
+
+  });
 </script>
