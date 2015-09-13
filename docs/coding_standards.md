ffbaa1db689483e73f226acbcf9977ff0d8e951f
underdogg <drosbeds@xs4all.nl>
2015-09-13 16:10:59 +0200

    Some more work and a little exception handling If a database error occurs, it gets caught separately from the other exceptions

---
+++ docs/coding_standards.md
@@ -0,0 +1,19 @@
+Standards
+====================
+
+Spaces or Tabs
+----------------
+Let's use spaces. 2 spaces is 1 tab (no comment)
+
+PS2
+------------------
+Let's use PS2 coding style
+
+function () {
+}
+
+and
+
+if() {
+}
+(no comment)
--- public/index.php
+++ public/index.php
@@ -2,22 +2,22 @@
 
 define('APP_PATH', realpath('../app/'));
 
-require_once APP_PATH.'/config/config.php';
-require_once APP_PATH.'/util/Auth.php';
+require_once APP_PATH . '/config/config.php';
+require_once APP_PATH . '/util/Auth.php';
 
 // Also spl_autoload_register (Take a look at it if you like)
 function __autoload($class) {
-    require LIBS . $class .".php";
+  require LIBS . $class . ".php";
 }
 
 
 // Load the Bootstrap!
 $bootstrap = new Bootstrap();
 
-// Optional Path Settings
-//$bootstrap->setControllerPath();
-//$bootstrap->setModelPath();
-//$bootstrap->setDefaultFile();
-//$bootstrap->setErrorFile();
-
-$bootstrap->init();
+try {
+  $bootstrap->init();
+} catch (PDOException $e) {
+  echo "<h2>DataBase Error</h2><br />".$e->getMessage();
+} catch (Exception $e) {
+  echo "<h2>General Error</h2><br />".$e->getMessage();
+}
