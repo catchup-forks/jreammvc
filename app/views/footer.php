1b05dfa20ee54ee0b26b9b0fe220bde5ae840e1a
underdogg <drosbeds@xs4all.nl>
2015-09-13 19:18:44 +0200

    Adding Bootstrap CSS

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,8 +1,12 @@
-</div>
 
-<div id="footer">
-    (C) Footer
-</div>
 
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
 </body>
 </html>
--- app/views/header.php
+++ app/views/header.php
@@ -1,46 +1,94 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title><?=(isset($this->title)) ? $this->title : 'MVC'; ?></title>
-    <link rel="stylesheet" href="<?php echo URL; ?>/css/default.css" />
-    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/sunny/jquery-ui.css" />
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="<?php echo URL; ?>public/js/custom.js"></script>
-    <?php 
-    if (isset($this->js)) 
-    {
-        foreach ($this->js as $js)
-        {
-            echo '<script type="text/javascript" src="'.URL.'views/'.$js.'"></script>';
-        }
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Bootstrap</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <!-- <link href="css/style.css" rel="stylesheet"> -->
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <script type="text/javascript" src=""></script>
+  <script type="text/javascript" src="<?php echo URL; ?>public/js/custom.js"></script>
+  <?php
+  if (isset($this->js)) {
+    foreach ($this->js as $js) {
+      echo '<script type="text/javascript" src="' . URL . 'views/' . $js . '"></script>';
     }
-    ?>
+  }
+  ?>
+
+
 </head>
+
 <body>
 
 <?php Session::init(); ?>
-    
-<div id="header">
 
-    <?php if (Session::get('loggedIn') == false):?>
-        <a href="<?php echo URL; ?>index">Index</a>
-        <a href="<?php echo URL; ?>help">Help</a>
-    <?php endif; ?>    
-    <?php if (Session::get('loggedIn') == true):?>
-        <a href="<?php echo URL; ?>dashboard">Dashboard</a>
-        <a href="<?php echo URL; ?>note">Notes</a>
-        
-        <?php if (Session::get('role') == 'owner'):?>
-        <a href="<?php echo URL; ?>user">Users</a>
-        <?php endif; ?>
-        
-        <a href="<?php echo URL; ?>dashboard/logout">Logout</a>    
-    <?php else: ?>
-        <a href="<?php echo URL; ?>login">Login</a>
-    <?php endif; ?>
-</div>
-    
-<div id="content">
-    
-    
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+              aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Bootstrap theme</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <?php if (Session::get('loggedIn') == true): ?>
+        <ul class="nav navbar-nav">
+
+          <li class="active"><a href="<?php echo URL; ?>dashboard">Dashboard</a></li>
+          <li><a href="<?php echo URL; ?>note">Notes</a></li>
+          <li><a href="<?php echo URL; ?>user">Users</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li class="dropdown-header">Nav header</li>
+              <li><a href="#">Separated link</a></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+        </ul>
+      <?php else: ?>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="<?php echo URL; ?>login">Login</a></li>
+        </ul>
+      <?php endif; ?>
+    </div>
+    <!--/.nav-collapse -->
+  </div>
+</nav>
+
+<div><BR /></div>
+
+
+<div class="container">
+
+
+
